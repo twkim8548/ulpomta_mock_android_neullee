7e67b23a8ce9a843d1bf3dc9159db425cf608603
sky6592 <789_skymert@naver.com>
2020-06-29 14:38:49 +0900

    neullee - 0629 - File generation

---
+++ Passion/app/src/main/java/com/example/passion/src/main/MainService.java
@@ -0,0 +1,39 @@
+package com.example.passion.src.main.models;
+
+import com.softsquared.template.src.main.interfaces.MainActivityView;
+import com.softsquared.template.src.main.interfaces.MainRetrofitInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.softsquared.template.src.ApplicationClass.getRetrofit;
+
+class MainService {
+    private final MainActivityView mMainActivityView;
+
+    MainService(final MainActivityView mainActivityView) {
+        this.mMainActivityView = mainActivityView;
+    }
+
+    void getTest() {
+        final MainRetrofitInterface mainRetrofitInterface = getRetrofit().create(MainRetrofitInterface.class);
+        mainRetrofitInterface.getTest().enqueue(new Callback<DefaultResponse>() {
+            @Override
+            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+                final DefaultResponse defaultResponse = response.body();
+                if (defaultResponse == null) {
+                    mMainActivityView.validateFailure(null);
+                    return;
+                }
+
+                mMainActivityView.validateSuccess(defaultResponse.getMessage());
+            }
+
+            @Override
+            public void onFailure(Call<DefaultResponse> call, Throwable t) {
+                mMainActivityView.validateFailure(null);
+            }
+        });
+    }
+}
